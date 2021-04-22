1193bd27fa4749e70bdb7b67098e74663c63cd6f
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-22 09:46:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-const calender = document.createElement("calender");
-const Sunday = document.createElement("Weekdays");
-// const nine = document.createElement("01");
-// const ten = document.createElement("02");
-// const eleven = document.createElement("03");
-// const twelve = document.createElement("04");
-// const one = document.createElement("05");
-// const two = document.createElement("06");
-// const three = document.createElement("07");
-// const four = document.createElement("08");
-// const five = document.createElement("09");
+//Date 
+$("#currentDay").text(luxon.DateTime.local().toLocaleString({
+    weekday: "long",
+    month: "long",
+    day: "2-digit",
+  })
+  );   
+  
+function time(){
+let fixedHour = parseInt(moment().format("H"));
+  
+//Determine timeblock color based on am or pm
+$(".time-block").each(function(){
+let presentHour = parseInt($(this).attr("id"));
+  
+ if(fixedHour > presentHour){
+   $(this).addClass("past");
+ } else if (presentHour === fixedHour){
+   $(this).addClass("present");
+ } else { (presentHour < fixedHour)
+   $(this).addClass("future");
+ }
+ });
+ }; 
+  
+  var saveBtn = $(".saveBtn")
+  saveBtn.on("click", function() {
 
-const date = new Date();
+  
+  let timeSchedule = $(this).siblings(".hour").text();
+  let text = $(this).siblings("#textarea-input").val();
+  
+  localStorage.setItem(timeSchedule, text);
+  });
+  
+  function storedAgenda (){
+  $(".hour").each(function(){
+    let presentHour = $(this).text();
+    let storedAgenda = localStorage.getItem(presentHour);
+  
+    if (storedAgenda !== null) {
+      $(this).siblings("textarea").val(storedAgenda);
+     }
+  })
+  }
 
-
-const month = date.getMonth();
-
-console.log(month);
-// [
-//     "January",
-//     "February",
-//     "March",
-//     "April",
-//     "May",
-//     "June",
-//     "July",
-//     "August",
-//     "September",
-//     "October",
-//     "November",
-//     "December",
-// ];
-
-// document.querySelector('monthName.h1').innerHTML = months [12];
-
-// const saveBtn = $(".saveBtn");
+  //Call functions
+  time();
+  storedAgenda();
