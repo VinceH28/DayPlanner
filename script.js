55527a06cbd38d20a892e5041267b65466621368
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-24 14:47:38 -0400

    update

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
-//Date 
 $("#currentDay").text(
   luxon.DateTime.local().toLocaleString({
     weekday: "long",
     month: "long",
     day: "2-digit",
   })
-);   
+);
 
-function time(){
-let currentTime = luxon.DateTime.local().toLocaleString({
-  hour: "2-digit",
-  hour12: false,
-});
+function timeChange() {
+  let currentTime = luxon.DateTime.local().toLocaleString({
+    hour: "2-digit",
+    hour12: false,
+  });
+  //console.log(currentTime);
   let formattedHour = parseInt(currentTime[0] + currentTime[1]);
 
-  $(".time-block").each(function(){
-      let currentHour = parseInt($(this).attr("id"));
+  $(".time-block").each(function () {
+    //console.log(this);
+    let currentHour = parseInt($(this).attr("id"));
 
-      if(formattedHour === currentHour){
-          $(this).addClass("present");
-      } else if (formattedHour < currentHour) {
-          $(this).addClass("future");
-      } else formattedHour > currentHour;
-      {
-          $(this).addClass("past");
-      }
-})
-};
+    if (formattedHour === currentHour) {
+      $(this).addClass("present");
+    } else if (formattedHour < currentHour) {
+      $(this).addClass("future");
+    } else formattedHour > currentHour;
+    {
+      $(this).addClass("past");
+    }
+  });
 
-var saveBtn = $(".saveBtn")
-saveBtn.on("click", function() {
-
-
-let hour = $(this).siblings(".hour").text();
-let input = $(this).siblings("#input").val();
-
-localStorage.setItem(hour, input);
-});
-
-function Store (){
-$(".hour").each(function(){
-  let time = $(this).text();
-  let storedAgenda = localStorage.getItem(time);
-
-  if (storedAgenda !== null) {
-    $(this).siblings("textarea").val(storedAgenda);
-   }
-});
+  //console.log(currentTime);
 }
 
-//Call functions
-time();
-Store();
+var saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function () {
+  let hour = $(this).siblings(".hour").text();
+  let input = $(this).siblings("#input").val();
+  // console.log(input);
+  // console.log(hour);
+
+  localStorage.setItem(hour, input);
+});
+
+function Storing (){
+    $(".hour").each(function(){
+        let time = $(this).text();
+        let text = localStorage.getItem(time);
+    if (text !== null) {
+        $(this).siblings("textarea").val(text);
+    }
+    });
+}
+timeChange();
+Storing();
