3c012026b8c17d5bf8f76ce3f9da745454b5f43d
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-24 08:41:20 -0400

    update

--- script.js
+++ script.js
@@ -1,49 +1,53 @@
 //Date 
 $("#currentDay").text(luxon.DateTime.local().toLocaleString({
-    weekday: "long",
-    month: "long",
-    day: "2-digit",
-  })
-  );   
-  
+  weekday: "long",
+  month: "long",
+  day: "2-digit",
+})
+);   
+
 function time(){
-let fixedHour = parseInt(moment().format("H"));
-  
-//Determine timeblock color based on am or pm
-$(".time-block").each(function(){
-let presentHour = parseInt($(this).attr("id"));
-  
- if(fixedHour > presentHour){
-   $(this).addClass("past");
- } else if (presentHour === fixedHour){
-   $(this).addClass("present");
- } else { (presentHour < fixedHour)
-   $(this).addClass("future");
- }
- });
- }; 
-  
-  var saveBtn = $(".saveBtn")
-  saveBtn.on("click", function() {
+let currentTime = luxon.DateTime.local().toLocaleString({
+  hour: "2-digit",
+  hour12: false,
+});
 
-  
-  let timeSchedule = $(this).siblings(".hour").text();
-  let text = $(this).siblings("#textarea-input").val();
-  
-  localStorage.setItem(timeSchedule, text);
-  });
-  
-  function storedAgenda (){
-  $(".hour").each(function(){
-    let presentHour = $(this).text();
-    let storedAgenda = localStorage.getItem(presentHour);
-  
-    if (storedAgenda !== null) {
-      $(this).siblings("textarea").val(storedAgenda);
-     }
-  })
+let formattedHour = parseInt(currentTime[0] + currentTime[1]);
+
+$(".time-block").each(function () {
+
+  let currentHour = parseInt($(this).attr("id"));
+
+  if (formattedHour === currentHour) {
+    $(this).addClass("present");
+  } else if (formattedHour < currentHour) {
+    $(this).addClass("future");
+  } else formattedHour > currentHour;
+  {
+    $(this).addClass("past");
   }
+});
 
-  //Call functions
-  time();
-  storedAgenda();
+}
+
+var saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function () {
+let hour = $(this).siblings(".hour").text();
+let input = $(this).siblings("#input").val();
+// console.log(input);
+// console.log(hour);
+
+localStorage.setItem(hour, input);
+});
+
+function Storing (){
+  $(".hour").each(function(){
+      let time = $(this).text();
+      let text = localStorage.getItem(time);
+  if (text !== null) {
+      $(this).siblings("textarea").val(text);
+  }
+  });
+}
+time();
